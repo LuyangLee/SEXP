7aadbc773d46173970857f48049af494ba083375
YangLeee <1021052838@qq.com>
2017-06-08 17:04:52 +0800

    my first add

---
+++ src/project1.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+struct circle
+{
+    double x;       //Ô²ÐÄx×ø±ê
+    double y;		//Ô²ÐÄy×ø±ê
+    double r;		//Ô²°ë¾¶
+};
+
+void theNocircle()
+{
+	
+} 
